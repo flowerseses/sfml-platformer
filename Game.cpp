843b3bee385477ae167a590f2d938883823012f3
Ivan Dervisevic <thedervish@gmail.com>
2016-11-07 23:10:47 +0100

    Hacky solutions for ground, jumping and (bugged) doublejumps
    Next up:
     - actual tiles
     - ground + ground collision
     - moving player stuff away from the main game loop
     - fixing dumbs
     - proper physics system?

--- Game.cpp
+++ Game.cpp
@@ -1,9 +1,26 @@
 #include "include/Game.hpp"
+#include <stdio.h>
 
 Game::Game()
   : mWindow(sf::VideoMode(800, 600), "Platforms"),
-    TimePerFrame(sf::seconds(1.f/60.f))
+    TimePerFrame(sf::seconds(1.f/60.f)),
+    playerSprite(),
+    playerPosition(400.f, ground),
+    mIsMovingLeft(false),
+    mIsMovingRight(false),
+    mIsJumping(false),
+    hasJumped(false),
+    hasDoubleJumped(false),
+    canDoubleJump(false),
+    playerSpeed(0.f, 0.f),
+    gravity(0.f, 4.f),
+    drag(0.5f, 0.f)
 {
+  if (!playerTexture.loadFromFile("textures/player_sprite.png")) {
+    //??
+  }
+  playerSprite.setTexture(playerTexture);
+  playerSprite.setPosition(playerPosition);
   // Do nothing for now.
 }
 
@@ -15,7 +32,7 @@ void Game::run() {
     while (timeSinceLastUpdate > TimePerFrame) {
       timeSinceLastUpdate -= TimePerFrame;
       processEvents();
-      update(TimePerFrame);
+      update(TimePerFrame, playerPosition, playerSpeed);
     }
     render();
   }
@@ -39,20 +56,71 @@ void Game::processEvents() {
   }
 }
 
-void Game::update(sf::Time deltatime) {
+void Game::update(sf::Time deltaTime, sf::Vector2f &playerPos, sf::Vector2f &playerSpeed) {
   sf::Vector2f speed(0.f, 0.f);
 
-  // Handle movement here!
+  if (mIsMovingLeft) {
+    speed.x -= moveSpeed;
+  }
+  if (mIsMovingRight) {
+    speed.x += moveSpeed;
+  }
+  if (mIsJumping) {
+    // Handle jumpstates
+    if (!hasJumped) {
+      hasJumped = true;
+      speed.y -= jumpSpeed*5;
+      speed.x *= 1.5;
+    } else if (canDoubleJump && !hasDoubleJumped) {
+      hasDoubleJumped = true;
+      canDoubleJump = false;
+      speed.y -= jumpSpeed*5;
+      speed.x *= 1.5;
+    }
+  } else if (playerPos.y < ground) {
+    speed += gravity;
+  } else if (playerPos.y > ground) {
+    hasJumped = false;
+    hasDoubleJumped = false;
+    canDoubleJump = false;
+    playerSpeed.y = 0.f;
+    speed.y = 0.f;
+    playerPos.y = ground;
+  }
+  /*float mod = 1.f;
+  if (playerSpeed.x > 0) {
+    mod = -1.f;
+    }*/
+  playerSpeed += speed;
+  playerSpeed.x = speed.x;
+  /*  if (abs(playerSpeed.x) > maxSpeed) {
+    playerSpeed.x = maxSpeed;
+    if (playerSpeed.x < 0) {
+      playerSpeed.x *= -1;
+    }
+    }*/
+  playerPos += playerSpeed* deltaTime.asSeconds()*10.f;
+  playerSprite.setPosition(playerPos);
 }
 
 void Game::render() {
   mWindow.clear();
   //mWindow.draw(things);
+  mWindow.draw(playerSprite);
   mWindow.display();
 }
 
 void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed) {
   if (key == sf::Keyboard::Escape) {
     mWindow.close();
+  } else if (key == sf::Keyboard::Space) {
+    mIsJumping = isPressed;
+    if (!mIsJumping && hasJumped && !hasDoubleJumped && !canDoubleJump) {
+      canDoubleJump = true;
+    }
+  } else if (key == sf::Keyboard::A || key == sf::Keyboard::Left) {
+    mIsMovingLeft = isPressed;
+  } else if (key == sf::Keyboard::D || key == sf::Keyboard::Right) {
+    mIsMovingRight = isPressed;
   }
  }
--- include/Game.hpp
+++ include/Game.hpp
@@ -11,16 +11,34 @@ public:
 
 private:
   void processEvents();
-  void update(sf::Time deltaTime);
+  void update(sf::Time deltaTime, sf::Vector2f &playerPos, sf::Vector2f &playerSpeed);
   void render();
-  void handlePlayerInput(sf::Keyboard::Key, bool);
+  void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
   void loadResources();
+  sf::Vector2f applyForces(sf::Vector2f playerSpeed, sf::Vector2f forces);
 
 private:
   sf::RenderWindow mWindow;
   sf::Time TimePerFrame;
-  //sf::Sprite playerSprite;
-  //sf::Sprite wallsSprite;
+  sf::Texture playerTexture;
+  sf::Texture wallsTexture;
+  sf::Sprite playerSprite;
+
+  // This is really ugly, move me out to the player controller maybe?
+  sf::Vector2f playerPosition;
+  sf::Vector2f playerSpeed;
+  bool mIsMovingLeft;
+  bool mIsMovingRight;
+  bool mIsJumping;
+  bool hasJumped;
+  bool hasDoubleJumped;
+  bool canDoubleJump;
+  static const float moveSpeed = 15.f;
+  static const float jumpSpeed = 10.f;
+  static const float maxSpeed = 12.f;
+  static const float ground = 400.f;
+  sf::Vector2f gravity;
+  sf::Vector2f drag;
 
 };
 
